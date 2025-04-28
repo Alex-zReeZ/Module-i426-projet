c5686348cbc6a2c833afe1965c1958e9054119ff
Henrique Rocha <henrique.pinto@eduvaud.ch>
2025-04-28 12:48:08 +0200

    Supprimé commentaires inutiles

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'root',
   standalone: true,
-  imports: [MatButtonModule],  // Import Material Button module
-  templateUrl: './home.component.html',  // Link your HTML template
+  imports: [MatButtonModule],
+  templateUrl: './home.component.html'
 })
 export class AppComponent {}
