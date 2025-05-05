5ff806426ef38ddd42adcfcff0b2bef8ddca15f4
Henrique-ecole <henrique.pinto@eduvaud.ch>
2025-05-05 08:36:48 +0200

    fix: fixed routing for every page

--- src/app/home-page/home.component.ts
+++ src/app/home-page/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'root',
   standalone: true,
-  imports: [MatButtonModule, RouterOutlet],
+  imports: [MatButtonModule, RouterOutlet, RouterLink],
   templateUrl: './home.component.html'
 })
 
