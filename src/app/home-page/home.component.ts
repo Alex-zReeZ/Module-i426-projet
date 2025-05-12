7b45b748a6106dd923c040f081b33ec7bd280b82
Henrique-ecole <henrique.pinto@eduvaud.ch>
2025-05-12 09:54:52 +0200

    Improved home UI

--- src/app/home-page/home.component.ts
+++ src/app/home-page/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { PageTitle } from '../page-title/page-title.component';
 
 @Component({
   selector: 'root',
   standalone: true,
-  imports: [MatButtonModule, RouterOutlet, RouterLink],
-  templateUrl: './home.component.html'
+  imports: [MatButtonModule, RouterLink, PageTitle],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 
 export class HomeComponent {}
