94afd7c75d60bd2a4b01f534fbbe6af5fae6c32b
alex-zreez <alexbeck220@gmail.com>
2025-05-14 18:21:32 +0200

    feat: created page for hikes list

--- src/app/hikes-search-list/hikes-search-list.component.ts
+++ src/app/hikes-search-list/hikes-search-list.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-hikes-search-list',
-  imports: [RouterOutlet],
   templateUrl: './hikes-search-list.component.html',
-  styleUrl: './hikes-search-list.component.scss'
+  styleUrl: './hikes-search-list.component.scss',
 })
+
 export class HikesSearchListComponent {}
