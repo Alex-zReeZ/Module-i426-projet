21085f600b6279c4e211a76e932d274e34d7f7e9
Alex Beck <alexbeck220@gmail.com>
2025-05-17 16:23:09 +0200

    added working navbar compoenent and implemented it in certain components

--- src/app/hikes-search-list/hikes-search-list.component.ts
+++ src/app/hikes-search-list/hikes-search-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { PageTitle } from '../page-title/page-title.component';
 
 @Component({
   selector: 'app-hikes-search-list',
   templateUrl: './hikes-search-list.component.html',
   styleUrl: './hikes-search-list.component.scss',
+  imports: [PageTitle],
+  standalone: true,
 })
 
 export class HikesSearchListComponent {}
