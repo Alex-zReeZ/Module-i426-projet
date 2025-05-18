fe51efb97692c3e40e4935b57508bce5cf0e8b20
Alex Beck <alexbeck220@gmail.com>
2025-05-18 14:01:15 +0200

    fetch hikes dynamically and display list

--- src/app/hikes-search-list/hikes-search-list.component.ts
+++ src/app/hikes-search-list/hikes-search-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { PageTitle } from '../page-title/page-title.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hikes-search-list',
   templateUrl: './hikes-search-list.component.html',
-  styleUrl: './hikes-search-list.component.scss',
-  imports: [PageTitle],
+  styleUrls: ['./hikes-search-list.component.scss'],
   standalone: true,
+  imports: [CommonModule, PageTitle, RouterModule],
 })
 
-export class HikesSearchListComponent {}
+export class HikesSearchListComponent implements OnInit {
+  hikes: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/walk').subscribe((data) => {
+      if (Array.isArray(data)) {
+        this.hikes = data;
+      } else if (data && typeof data === 'object') {
+        this.hikes = Object.entries(data).map(([id, hike]: [string, any]) => ({
+          id,
+          ...hike,
+        }));
+      } else {
+        this.hikes = [];
+      }
+    });
+  }
+}
