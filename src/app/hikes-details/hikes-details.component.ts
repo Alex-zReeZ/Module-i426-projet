9d78ac7103271677b72351d8b78d9c3494047dee
alexb <alexbeck220@gmail.com>
2025-05-18 16:58:17 +0200

    feat: added working dynamic components

--- src/app/hikes-details/hikes-details.component.ts
+++ src/app/hikes-details/hikes-details.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PageTitle } from '../page-title/page-title.component';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-hikes-details',
-  imports: [PageTitle],
-  templateUrl: './hikes-details.component.html',
-  styleUrl: './hikes-details.component.scss'
+    selector: 'app-hikes-details',
+    imports: [CommonModule, PageTitle],
+    templateUrl: './hikes-details.component.html',
+    styleUrl: './hikes-details.component.scss'
 })
-export class HikesDetailsComponent {}
+export class HikesDetailsComponent implements OnInit {
+    hike: any;
+    safeMapUrl: SafeResourceUrl | null = null;
+
+    constructor(
+        private route: ActivatedRoute,
+        private http: HttpClient,
+        private sanitizer: DomSanitizer
+    ) {}
+
+    ngOnInit() {
+        const id = this.route.snapshot.paramMap.get('id');
+        this.http.get<any>(`http://localhost:3000/walk?id=${id}`).subscribe(data => {
+            console.log('Données reçues :', data);
+            // Si c'est un tableau, prends le premier élément :
+            this.hike = Array.isArray(data) ? data[0] : data;
+            this.safeMapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.hike.mapEmbedUrl);
+        });
+    }
+
+}
